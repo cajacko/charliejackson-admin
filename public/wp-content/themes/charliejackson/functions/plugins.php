3267f8bffc9bb2fca504b20351e1132810092fa4
Charlie Jackson <contact@charliejackson.com>
2016-07-10 20:51:21 +0100

    Updated the evernote importer

--- public/wp-content/themes/charliejackson/functions/plugins.php
+++ public/wp-content/themes/charliejackson/functions/plugins.php
@@ -4,4 +4,5 @@ function charliejackson_plugins()
 {
     activate_plugin('wordpress-twitter-importer/wordpress-twitter-importer.php');
     activate_plugin('wordpress-github-importer/wordpress-github-importer.php');
+    activate_plugin('wordpress-github-importer/wordpress-evernote-importer.php');
 }
